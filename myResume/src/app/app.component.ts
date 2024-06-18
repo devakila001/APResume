a330f0429769ab70dfbff9f1b1587f1584b45b40
Akila Peramatchi <dev.akila001@outlook.com>
2024-06-18 22:44:27 +0530

    Created a components

--- myResume/src/app/app.component.ts
+++ myResume/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectComponent } from './project/project.component';
+import { EducationComponent } from './education/education.component';
+import { CourseComponent } from './course/course.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,SummaryComponent, AchievementsComponent,
+  ExperienceComponent, ProjectComponent, EducationComponent, CourseComponent, SkillsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
